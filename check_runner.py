f384b7701eb83759aaeddef11eb0898a675b6750
Jakub Pawlikowski <pawlikoj@mcmaster.ca>
2023-02-03 22:57:23 -0500

    updates and format

--- check_runner.py
+++ check_runner.py
@@ -1,42 +1,41 @@
 
 
-upc = '03600024145'
+#upc = '03600024145'
+
+upc = '103600024145'
 
 upc_length = len(upc)
 
 print("Current UPC (without check digit): " + upc + "  ->  Len: " + str(upc_length))
 
-if upc_length == 11:
-    print("Calculating check digit for length 12")
-    upc_reversed = []
-    for i in range(1, upc_length):
-        upc_reversed.append(upc[i*(-1)])
-    upc_reversed.append(upc[0])
-    #print(str(upc_reversed))
+print("Calculating check digit for length : " + str(upc_length) + " ..")
 
-    even_digits = []
-    odd_digits = []
-    for i in range(0, upc_length):
-        if i % 2 == 0: #switched even and off due to position count starting from 1
-            odd_digits.append(upc_reversed[i])
-        else:
-            even_digits.append(upc_reversed[i])
-    #print(str(odd_digits))
-    #print(str(even_digits))
+upc_reversed = []
+for i in range(1, upc_length):
+    upc_reversed.append(upc[i*(-1)])
+upc_reversed.append(upc[0])
 
-    odd_product = 0
-    for i in odd_digits:
-        odd_product = odd_product + int(i)
+even_digits = []
+odd_digits = []
+for i in range(0, upc_length):
+    if i % 2 == 0: #switched even and off due to position count starting from 1
+        odd_digits.append(upc_reversed[i])
+    else:
+        even_digits.append(upc_reversed[i])
 
-    odd_product = odd_product * 3
+odd_product = 0
+for i in odd_digits:
+    odd_product = odd_product + int(i)
 
-    even_sum = 0
-    for i in even_digits:
-        even_sum = even_sum + int(i)
+odd_product = odd_product * 3
 
-    combined_sum = odd_product + even_sum
+even_sum = 0
+for i in even_digits:
+    even_sum = even_sum + int(i)
 
-    check_digit = 10 - (combined_sum % 10)
+combined_sum = odd_product + even_sum
 
-    print("Calculated check digit : " + str(check_digit))
-    print("Combined UPC : " + upc + str(check_digit))
+check_digit = 10 - (combined_sum % 10)
+
+print("Calculated check digit : " + str(check_digit))
+print("Combined UPC : " + upc + str(check_digit))
